eb670e55a45aecb706a46ccdc2ed993297c667d2
JoaoPPradela <joao.pradela@aluno.senai.br>
2026-02-03 11:26:18 -0300

    feat e sytle.css: Agenda com uma estilização básica

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template, request, redirect, url_for
 from models.tarefa import Tarefa
 
 app = Flask(__name__)
+
+# Lista Global para guardar as tarefas
+lista_de_tarefas = [] 
 
 @app.route('/')
 def home():
@@ -11,14 +12,24 @@ def home():
 
 @app.route('/agenda', methods=['GET','POST'])
 def agenda():
-    tarefa = None
-
     if request.method == 'POST':
-     titulo_tarefa = request.form['titulo-tarefa']
-     data_conclusao = request.form['data-conclusao']
-     tarefa = Tarefa(titulo_tarefa, data_conclusao)
-     tarefa.salvar_tarefa()
-    return render_template('agenda.html', titulo='Agenda', tarefa=tarefa)
+        titulo_tarefa = request.form['titulo-tarefa']
+        data_conclusao = request.form['data-conclusao']
+        
+        # Cria a tarefa e salva
+        nova_tarefa = Tarefa(titulo_tarefa, data_conclusao)
+        nova_tarefa.salvar_tarefa()
+        
+        # Adiciona na lista global
+        lista_de_tarefas.append(nova_tarefa)
+        return redirect(url_for('agenda'))
+
+    # Se for GET (apenas carregando a página), mostramos a lista
+    return render_template('agenda.html', titulo='Agenda', tarefas=lista_de_tarefas)
+
 @app.route('/ola')
 def ola_mundo():
     return "Olá, Mundo!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
