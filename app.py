2bacebe4e79f698d4e670d8b3cac6f6a8a3f5d7c
JoaoPPradela <joao.pradela@aluno.senai.br>
2026-02-03 16:45:30 -0300

    feat: Deletando tarefas

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
 from models.tarefa import Tarefa
-
 app = Flask(__name__)
-
-# Lista Global para guardar as tarefas
-lista_de_tarefas = [] 
 
 @app.route('/')
 def home():
@@ -12,24 +8,25 @@ def home():
 
 @app.route('/agenda', methods=['GET','POST'])
 def agenda():
+    tarefas = None
+
     if request.method == 'POST':
         titulo_tarefa = request.form['titulo-tarefa']
         data_conclusao = request.form['data-conclusao']
-        
-        # Cria a tarefa e salva
-        nova_tarefa = Tarefa(titulo_tarefa, data_conclusao)
-        nova_tarefa.salvar_tarefa()
-        
-        # Adiciona na lista global
-        lista_de_tarefas.append(nova_tarefa)
-        return redirect(url_for('agenda'))
+        tarefa = Tarefa(titulo_tarefa, data_conclusao)
+        tarefa.salvar_tarefa()
 
-    # Se for GET (apenas carregando a página), mostramos a lista
-    return render_template('agenda.html', titulo='Agenda', tarefas=lista_de_tarefas)
+    tarefas = Tarefa.obter_tarefas()
+    return render_template('agenda.html', titulo='Agenda', tarefas=tarefas)
+
+  
+@app.route('/delete/<int:idTarefa>')
+def delete(idTarefa):
+    tarefa = Tarefa.id(idTarefa)
+    tarefa.excluir_tarefa()
+    return
+
 
 @app.route('/ola')
 def ola_mundo():
     return "Olá, Mundo!"
-
-if __name__ == '__main__':
-    app.run(debug=True)
